d2e22fa8ffc6977471fd79e49daaa24821a35802
VitorRusch <rusch.profissional@gmail.com>
2025-01-03 16:20:31 -0300

    Footer changes

--- src/components/global/footer.tsx
+++ src/components/global/footer.tsx
@@ -4,72 +4,130 @@ import Icons from '../global/icons'
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col relative items-center justify-center border-t border-border pt-16 pb-8 px-6 lg:px-8 w-full max-w-6xl mx-auto lg:pt-32">
-      <div className="grid gap-8 xl:grid-cols-3 xl:gap-8 w-full">
-        <div className="flex flex-col items-start justify-start md:max-w-[200px]">
-          <div className="flex items-start">
+    <footer className="flex flex-col relative items-center justify-center border-t border-border pt-16 pb-8 px-6 lg:px-8 w-full max-w-6xl mx-auto lg:pt-32 bg-[#ffcbdb]">
+      <div className="grid gap-8 xl:grid-cols-5 xl:gap-8 w-full">
+        <div className="grid-cols-4 gap-6 grid xl:col-span-2 xl:mt-0">
+          <div className="flex flex-row gap-1 items-center">
             <Icons.logo className="w-7 h-7" />
+            <p className="text-black text-sm text-start">Escort Site</p>
           </div>
-          <p className="text-black mt-4 text-sm text-start">Escort Site</p>
-        </div>
-
-        <div className="grid-cols-2 gap-8 grid mt-16 xl:col-span-2 xl:mt-0">
-          <div className="md:grid md:grid-cols-2 md:gap-8">
+          <div className="md:grid md:gap-8">
             <div className="mt-10 md:mt-0 flex flex-col">
-              <h3 className="text-base font-medium text-white">Integrations</h3>
-              <ul className="mt-4 text-sm text-black">
+              <h3 className="text-base font-medium text-black">Menu</h3>
+              <ul className="mt-1 text-sm text-black">
                 <li className="mt-2">
                   <Link
                     href=""
                     className="hover:text-foreground transition-all duration-300"
                   >
-                    Instagram
+                    Home
                   </Link>
                 </li>
-                <li className="mt-2">
+                <li className="mt-1">
                   <Link
                     href=""
                     className="hover:text-foreground transition-all duration-300"
                   >
-                    TikTok
+                    Membership & Pricing
+                  </Link>
+                </li>
+                <li className="mt-1">
+                  <Link
+                    href=""
+                    className="hover:text-foreground transition-all duration-300"
+                  >
+                    Sing up
+                  </Link>
+                </li>
+                <li className="mt-1">
+                  <Link
+                    href=""
+                    className="hover:text-foreground transition-all duration-300"
+                  >
+                    Login
                   </Link>
                 </li>
               </ul>
             </div>
           </div>
           <div className="mt-10 md:mt-0 flex flex-col">
-            <h3 className="text-base font-medium text-white">Company</h3>
-            <ul className="mt-4 text-sm text-black">
+            <h3 className="text-base font-medium text-black">Browse</h3>
+            <ul className="mt-2 text-sm text-black">
               <li className="">
                 <Link
                   href=""
                   className="hover:text-foreground transition-all duration-300"
                 >
-                  teste
+                  Locations
                 </Link>
               </li>
-              <li className="mt-2">
+              <li className="mt-1">
                 <Link
                   href=""
                   className="hover:text-foreground transition-all duration-300"
                 >
-                  teste
+                  All Escorts
                 </Link>
               </li>
-              <li className="mt-2">
+            </ul>
+          </div>
+          <div className="mt-10 md:mt-0 flex flex-col">
+            <h3 className="text-base font-medium text-black">Plataform</h3>
+            <ul className="mt-2 text-sm text-black">
+              <li className="">
                 <Link
                   href=""
                   className="hover:text-foreground transition-all duration-300"
                 >
-                  teste
+                  About
                 </Link>
               </li>
-              <li className="mt-2">
+              <li className="mt-1">
                 <Link
                   href=""
                   className="hover:text-foreground transition-all duration-300"
                 >
-                  Teste
+                  Help & Support
+                </Link>
+              </li>
+              <li className="mt-1">
+                <Link
+                  href=""
+                  className="hover:text-foreground transition-all duration-300"
+                >
+                  Social
+                </Link>
+              </li>
+              <li className="mt-1">
+                <Link
+                  href=""
+                  className="hover:text-foreground transition-all duration-300"
+                >
+                  Privacy
+                </Link>
+              </li>
+              <li className="mt-1">
+                <Link
+                  href=""
+                  className="hover:text-foreground transition-all duration-300"
+                >
+                  Legal Notices
+                </Link>
+              </li>
+              <li className="mt-1">
+                <Link
+                  href=""
+                  className="hover:text-foreground transition-all duration-300"
+                >
+                  Terms
+                </Link>
+              </li>
+              <li className="mt-1">
+                <Link
+                  href=""
+                  className="hover:text-foreground transition-all duration-300"
+                >
+                  Anti-Exploitation Policy
                 </Link>
               </li>
             </ul>
@@ -77,7 +135,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="mt-8 border-t border-border/40 pt-4 md:pt-8 md:flex md:items-center md:justify-between w-full">
+      <div className="border-t border-border/40 pt-4 md:pt-8 md:flex md:items-center md:justify-between w-full">
         <p className="text-sm text-black mt-8 md:mt-0">
           &copy; {new Date().getFullYear()} Escort Site. All rights reserved.
         </p>
