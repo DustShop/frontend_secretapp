b97908184afe51ddd893a34d4d30394f0f83aa29
VitorRusch <rusch.profissional@gmail.com>
2025-01-08 23:42:34 -0300

    Update Footer component styles for improved aesthetics and layout consistency

--- src/components/global/footer.tsx
+++ src/components/global/footer.tsx
@@ -6,8 +6,10 @@ import Icons from '../global/icons'
 const Footer = () => {
   
   return (
-    <footer className="flex flex-col relative items-center justify-center border-t border-border pt-16 pb-8 px-6 lg:px-8 w-screen mx-auto bg-[#ffcbdb]">
-      <div className="grid gap-8 xl:grid-cols-4 xl:gap-8 w-full pb-8">
+    <footer
+      className="flex flex-col relative items-center justify-center border-t border-border pt-16 pb-8 px-6 lg:px-8 w-screen mx-auto bg-[#ECEFF1]"
+    >
+      <div className="grid gap-8 xl:grid-cols-1 xl:gap-8 w-full pb-8">
         <div className="grid-cols-4 gap-6 grid xl:col-span-4 xl:mt-0">
           <div className="flex flex-row gap-1 items-center">
             <Icons.logo className="w-7 h-7" />
